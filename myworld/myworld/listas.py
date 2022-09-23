35a6f772e3f764d95baa7588bb9f87314d64f070
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-23 11:49:34 -0300

    commit changes

--- myworld/myworld/listas.py
+++ myworld/myworld/listas.py
@@ -2,15 +2,17 @@
 lista = ['RENATO', 'CREUZA ALVES', 'NATHANZINHO',
          'CAROLINE', 'RUBENS ALVES', 110, 220]
 
-print(len(lista))
+# print(len(lista))
 
-# lista.append('Rubiana Cavalcanti')
-lista.append(660)
+# # lista.append('Rubiana Cavalcanti')
+# lista.append(660)
+# print(lista)
+# # print(len(lista)-1)
+# # print(len(lista)-2)
+# # print(len(lista)-3)
+# # print(len(lista)-4)
+# del lista[0]
+# print(lista[5])
+# print(lista[0])
+lista.remove('NATHANZINHO')
 print(lista)
-# print(len(lista)-1)
-# print(len(lista)-2)
-# print(len(lista)-3)
-# print(len(lista)-4)
-del lista[0]
-print(lista)
-print(lista[0])
