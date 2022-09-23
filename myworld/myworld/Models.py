e6dc16d59390449ad618c1460096e3fd19462af7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-23 11:32:48 -0300

    commit changes

---
+++ myworld/myworld/Models.py
@@ -0,0 +1,24 @@
+# from django.db import models
+
+
+# class Reporter(models.Model):
+#     full_name = models.CharField(max_length=70)
+
+#     def __str__(self):
+#         return self.full_name
+
+
+# class Article(models.Model):
+#     pub_date = models.DateField()
+#     headline = models.CharField(max_length=200)
+#     content = models.TextField()
+#     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+# def __str__(self):
+#     return self.headline
+
+numero = 100
+frase = "renato alves  soares"
+print(frase.capitalize())
+print(numero.size())
+# print(dir(numero.from_bytes))
---
+++ myworld/myworld/listas.py
@@ -0,0 +1,9 @@
+
+lista = ['RENATO', 'CREUZA ALVES', 'NATHANZINHO',
+         'CAROLINE', 'RUBENS ALVES', 110, 220]
+
+print(len(lista))
+
+# lista.append('Rubiana Cavalcanti')
+lista.append(660)
+print(lista)
