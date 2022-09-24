0dd8ebb79b95689e939b7e0c9a25a43b07ae7c77
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-23 21:16:40 -0300

    ok

--- myworld/myworld/Models.py
+++ myworld/myworld/Models.py
@@ -1,30 +1,10 @@
 from django.db import models
 
 
-class Reporter(models.Model):
-    first_name = models.CharField(max_length=120)
-    full_name = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.full_name
-
-    def __str__(self):
-        return self.first_name
-
-
-class Article(models.Model):
-    pub_date = models.DateField()
-    headline = models.CharField(max_length=200)
-    content = models.TextField()
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
-
-
-def __str__(self):
-    return self.headline
-
-
-numero = 100
-frase = "renato alves  soares"
-print(frase.capitalize())
-print(numero.size())
-# print(dir(numero.from_bytes))
+class Employees(models.Model):
+    emp_no = models.IntegerField(primary_key=True)
+    birth_date = models.DateField()
+    first_name = models.CharField(max_length=14)
+    last_name = models.CharField(max_length=16)
+    gender = models.CharField(max_length=1)
+    hire_date = models.DateField()
