2c3f1ecc0d4a926f2e37dbef3bf2b93eb9114c82
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-24 18:03:59 -0300

    ok

--- myworld/myworld/interpolacao.py
+++ myworld/myworld/interpolacao.py
@@ -2,4 +2,5 @@ nome = "RENATO ALVES"
 
 idade = 51
 
-print("O {nome} tem {idade}  anos de idade". format(nome=nome, idade=idade))
+# print("O {nome} tem {idade}  anos de idade". format(nome=nome, idade=idade))
+print(f'O funcionário {nome} tem {idade} de anos de idade')
